40bb41215682e11c9a5fc17b0f3f6ac305edefb4
Dmitry Nekrasov <d04101995@mail.ru>
2015-06-22 16:22:44 +0300

    hw2.1

---
+++ Free.hs
@@ -0,0 +1,18 @@
+module Free where
+
+import Expression
+import Data.Set
+
+foldVariables :: String -> String -> String
+foldVariables [] b = b
+foldVariables a b  = a ++ "\n" ++ b
+
+freeVariables :: Expression -> String
+freeVariables = Prelude.foldl foldVariables [] . toList . expressionToSet
+
+expressionToSet :: Expression -> Set String
+expressionToSet (Var x) = singleton x
+expressionToSet (Lambda x a) = if member x (expressionToSet a)
+							   then delete x (expressionToSet a)
+							   else expressionToSet a
+expressionToSet (Apply a b) = union (expressionToSet a) (expressionToSet b)
---
+++ TT2.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import Expression
+import Reader
+import Free
+
+main = do
+	   input <- readFile "task2.in"
+	   writeFile "task2.out" $ freeVariables $ readLambda input
